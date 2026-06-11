5f5a2d5a38c200e771118ae3147421659d2458ed
Santi4000 <santiago.martinez.cuevas.5@gmail.com>
2026-06-11 01:30:57 -0300

    Update juego.h

--- juego.h
+++ juego.h
@@ -1,20 +1,6 @@
 #ifndef JUEGO_H
 #define JUEGO_H
 
-int movIzquierda(int **arr, int tam);
-int desplazarIz(int **arr, int tam);
-int fusionIz(int **arr, int tam);
-
-int movDerecha(int **arr, int tam);
-int desplazarDe(int **arr, int tam);
-int fusionDe(int **arr, int tam);
-
-int movArriba(int **arr, int tam);
-int desplazarAr(int **arr, int tam);
-int fusionAr(int **arr, int tam);
-
-int movAbajo(int **arr, int tam);
-int desplazarAb(int **arr, int tam);
-int fusionAb(int **arr, int tam);
+int moverLinea(int *arr, int tam);
 
 #endif
